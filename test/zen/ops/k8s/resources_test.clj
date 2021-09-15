69a6e4e58303148207f2fa4e86bbe4b0fdb430ab
Artur Sharipov <theartua@gmail.com>
2021-09-15 17:35:35 +0300

    fix resources api versions

--- test/zen/ops/k8s/resources_test.clj
+++ test/zen/ops/k8s/resources_test.clj
@@ -9,7 +9,6 @@
             [clojure.walk]
             [clojure.test :as t]))
 
-
 (t/deftest test-k8s-resources
   (def ztx (zen/new-context {}))
   (openapi/load-default-api ztx)
@@ -31,46 +30,44 @@
 
   ;;(t/is (empty? (take 10 (zen/errors ztx))))
 
-  (def app (zen/get-symbol ztx 'mysys/myapp))
 
+  (def app (zen/get-symbol ztx 'mysys/myapp))
 
   (comment
     (openapi/describe  ztx 'k8s.batch.api.k8s.io.v1/JobSpec)
     (openapi/describe ztx 'k8s.networking.api.k8s.io.v1/Ingress)
-    (openapi/describe ztx 'k8s.v1/Pod)
-    )
+    (openapi/describe ztx 'k8s.v1/Pod))
 
   (matcho/match
    (res/do-expand ztx app)
-   [{:k8s/type 'k8s.networking.api.k8s.io.v1/Ingress
-     :metadata {:name "myapp" :namespace "my"}
-     :spec {:rules [{:host "myapp.samurai.io"
-                     :http {:paths [{:backend {:service {:name "myapp" :port 80}}
-                                     :path "/"}]}}]}}
-    {:k8s/type 'k8s.v1/Service
-     :metadata {:name "myapp" :namespace "my"}
-     :spec {:selector {:app "myapp"}
-            :ports [{:protocol "TCP"
-                     :port 80
-                     :targetPort 8080}]}}
+    [{:k8s/type 'k8s.networking.api.k8s.io.v1/Ingress
+      :metadata {:name "myapp" :namespace "my"}
+      :spec {:rules [{:host "myapp.samurai.io"
+                      :http {:paths [{:backend {:service {:name "myapp" :port 80}}
+                                      :path "/"}]}}]}}
+     {:k8s/type 'k8s.v1/Service
+      :metadata {:name "myapp" :namespace "my"}
+      :spec {:selector {:app "myapp"}
+             :ports [{:protocol "TCP"
+                      :port 80
+                      :targetPort 8080}]}}
 
-    {:k8s/type 'k8s.apps.v1/Deployment
-     :metadata {:name "myapp" :namespace "my"}
-     :spec {:replicas 1
-            :selector {:matchLabels {:app "myapp"}}
-            :template {:metadata {:labels {:app "myapp"}}
-                       :spec {:containers [{:name "main"
-                                            :image "nginx:latest"
-                                            :ports [{:containerPort 8080}]}]}}}}])
+     {:k8s/type 'k8s.apps.v1/Deployment
+      :metadata {:name "myapp" :namespace "my"}
+      :spec {:replicas 1
+             :selector {:matchLabels {:app "myapp"}}
+             :template {:metadata {:labels {:app "myapp"}}
+                        :spec {:containers [{:name "main"
+                                             :image "nginx:latest"
+                                             :ports [{:containerPort 8080}]}]}}}}])
 
-  (openapi/list-schemas ztx "ingres")
+  (openapi/list-schemas ztx "cluster")
 
   (->>
    (res/do-expand ztx app)
    (mapcat (fn [res]
-           (:errors (zen/validate ztx #{(:k8s/type res)}
-                          (dissoc res :k8s/type))))))
-
+             (:errors (zen/validate ztx #{(:k8s/type res)}
+                                    (dissoc res :k8s/type))))))
 
   (zen/load-ns ztx {'ns 'mysys
 
@@ -86,87 +83,88 @@
 
   (matcho/match
    (res/do-expand ztx app)
-   
+
     [{:k8s/type 'k8s.v1/Service
       :metadata {:name "prometheus"
                  :namespace "monitoring"}}
-  
-     {:k8s/type 'k8s.rbac.authorization.k8s.io.v1/ClusterRole,
+
+     {:k8s/type 'k8s.rbac.api.k8s.io.v1/ClusterRole,
       :metadata {:name "prometheus"
                  :namespace "monitoring"},
       :rules
-      [{:apiGroups [ "" ],
-         :resources [ "nodes" "services" "endpoints" "pods" ],
-        :verbs [ "get" "list" "watch" ]}
-       {:apiGroups [ "" ], :resources [ "configmaps" ], :verbs [ "get" ]}
-       {:nonResourceURLs [ "/metrics" ], :verbs [ "get" ]}
-       {:apiGroups [ "" ], :resources [ "nodes/metrics" ], :verbs [ "get" ]}]}
-  
-     {:k8s/type 'k8s.rbac.authorization.k8s.io.v1/ClusterRoleBinding,
+      [{:apiGroups [""],
+        :resources ["nodes" "services" "endpoints" "pods"],
+        :verbs ["get" "list" "watch"]}
+       {:apiGroups [""], :resources ["configmaps"], :verbs ["get"]}
+       {:nonResourceURLs ["/metrics"], :verbs ["get"]}
+       {:apiGroups [""], :resources ["nodes/metrics"], :verbs ["get"]}]}
+
+     {:k8s/type 'k8s.rbac.api.k8s.io.v1/ClusterRoleBinding,
       :metadata {:name "prometheus"
                  :namespace "monitoring"},
       :roleRef
       {:apiGroup "rbac.authorization.k8s.io",
        :kind "ClusterRole",
        :name "prometheus"},
-      :subjects [{:kind "ServiceAccount", :name "prometheus"} ]}
-  
+      :subjects [{:kind "ServiceAccount", :name "prometheus"}]}
+
      {:k8s/type 'k8s.v1/Service
       :metadata {:name "prometheus"
                  :namespace "monitoring"},
       :spec
-      {:ports [ {:name "promui", :protocol "TCP", :port 9090, :targetPort 9090} ],
+      {:ports [{:name "promui", :protocol "TCP", :port 9090, :targetPort 9090}],
        :type "NodePort"}}
-  
+
      {:k8s/type 'k8s.v1/PersistentVolumeClaim
       :metadata {:name "prometheus-data"
                  :namespace "monitoring"},
       :spec
-      {:accessModes [ "ReadWriteOnce" ], :resources {:requests {:storage "100Gi"}}}}
-  
+      {:accessModes ["ReadWriteOnce"], :resources {:requests {:storage "100Gi"}}}}
+
      {:k8s/type 'k8s.apps.v1/Deployment
       :metadata {:name "prometheus"
                  :namespace "monitoring"},
       :spec
       {:replicas 1,
        :strategy {:type "Recreate"},
+       :selector {:matchLabels {:app "prometheus"}}
        :template
-       {:spec
+       {:metadata {:labels {:app "prometheus"}}
+        :spec
         {:serviceAccountName "prometheus",
          :securityContext {:runAsUser 1000, :fsGroup 1000},
          :containers
-         [ {:name "prometheus",
-           :command [ "/bin/prometheus" ],
+         [{:name "prometheus",
+           :command ["/bin/prometheus"],
            :args
-           [ "--config.file=/config/prometheus-config.yaml"
+           ["--config.file=/config/prometheus-config.yaml"
             "--storage.tsdb.path=/data/prometheus"
             "--storage.tsdb.retention.time=14d"
             "--web.console.libraries=/usr/share/prometheus/console_libraries"
             "--web.console.templates=/usr/share/prometheus/consoles"
             "--web.enable-lifecycle"
-            "--web.route-prefix=/" ],
+            "--web.route-prefix=/"],
            :image "prom/prometheus",
            :imagePullPolicy "Always",
            :resources {:requests {:memory "1Gi"}, :limits {:memory "1Gi"}},
-           :ports [ {:containerPort 9090} ],
+           :ports [{:containerPort 9090}],
            :volumeMounts
-           [ {:mountPath "/data/prometheus", :name "prometheus-data"}
-            {:mountPath "/config", :name "prometheus-config"} ]} ],
+           [{:mountPath "/data/prometheus", :name "prometheus-data"}
+            {:mountPath "/config", :name "prometheus-config"}]}],
          :volumes
-         [ {:name "prometheus-config", :configMap {:name "prometheus-config"}}
+         [{:name "prometheus-config", :configMap {:name "prometheus-config"}}
           {:name "prometheus-data",
-           :persistentVolumeClaim {:claimName "prometheus-data"}} ]}}}}
+           :persistentVolumeClaim {:claimName "prometheus-data"}}]}}}}
 
-{:k8s/type 'k8s.v1/ConfigMap
-    :metadata {:name "prometheus-config"
-               :namespace "monitoring"}
-    :data {:prometheus-config.yaml string?}}
-     ])
-
-  )
+     {:k8s/type 'k8s.v1/ConfigMap
+      :metadata {:name "prometheus-config"
+                 :namespace "monitoring"}
+      :data {:prometheus-config.yaml string?}}]))
 
 
 ;; grab schema from https://prometheus.io/docs/prometheus/latest/configuration/configuration/
+
+
 (def config
   {:op/type 'zen.ops.k8s/prometheus-config
    :global {:scrape_interval "15s", :evaluation_interval "15s"},
@@ -189,7 +187,6 @@
                                                :replacement "${1}"}]
                      :metrics_path "/metrics/cadvisor"
                      :scheme "https"}]})
-
 
 (def prometheus
   {:ops/type 'zen.ops.k8s/service
